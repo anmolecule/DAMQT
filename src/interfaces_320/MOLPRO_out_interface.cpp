ac823f48c36c5e7cd8d31a5f5e8c7d82bf65fdd9
rafa <rafaellopezfernandez@gmail.com>
2025-01-29 16:31:40 +0100

    MOLPRO interface updated to put the coordinates origin of a molecule in the center of nuclear charges. This setting had been changed to keep the original coordinates without translation, but it causes misfunctions when translating/rotating molecules if the coordinates origin is not placed that way.

--- src/interfaces_320/MOLPRO_out_interface.cpp
+++ src/interfaces_320/MOLPRO_out_interface.cpp
@@ -1151,8 +1151,8 @@ void readoptimizedgeometry(ifstream * inputfile, ofstream * outimportfile, ofstr
                 }
             }
 //            redefines the coordinates with respect to the center of positive charges and writes them to file .ggbs
-//             xc = xc / zntot; yc = yc / zntot; zc = zc / zntot;
-            xc = 0.; yc = 0.; zc = 0.;  // Keeps original coordinates without translation of origin to center of positive charges
+             xc = xc / zntot; yc = yc / zntot; zc = zc / zntot;
+//            xc = 0.; yc = 0.; zc = 0.;  // Keeps original coordinates without translation of origin to center of positive charges
             *ggbsfile << ncen << endl ;
             (*ggbsfile).setf(ios::scientific,ios::floatfield);
             for(i=0; i<ncen;i++){
